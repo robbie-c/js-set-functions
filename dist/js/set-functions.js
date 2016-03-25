46bf8d8d799f71bc8327cf0bbb3f506e15580d52
Robbie Coomber <robbie.coomber@gmail.com>
2016-03-25 18:27:55 +0000

    Update dependencies
    The biggest part of this was moving to babel 6. This required adding a .babelrc file.

--- dist/js/set-functions.js
+++ dist/js/set-functions.js
@@ -1,8 +1,12 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SetFunctions = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
 /**
  * JavaScript Set operations
  *
- * All functions expect an ES6 Set type or a compatible polyfill.
+ * All functions expect an ES6 Set type or a compatible polyfill to be available
+ * in the global namespace.
+ * @module set-functions
  */
 
 /*
@@ -28,8 +32,6 @@
  * @param {Set} b
  * @returns {Set}
  */
-'use strict';
-
 module.exports.intersection = function intersection(a, b) {
   'use strict';
 
@@ -154,8 +156,22 @@ module.exports.isEqual = function isEqual(a, b) {
   return true;
 };
 
-// Aliases
+/**
+ * Alias for union
+ *
+ * @param {Set} a
+ * @param {Set} b
+ * @returns {Set}
+ */
 module.exports.add = module.exports.union;
+
+/**
+ * Alias for difference
+ *
+ * @param {Set} a
+ * @param {Set} b
+ * @returns {Set}
+ */
 module.exports.subtract = module.exports.difference;
 
 },{}]},{},[1])(1)
